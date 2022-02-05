ae8d97d3d8d78e9b765106b042d369ac3e76a23b
Joaco95 <88909293+Joaco95@users.noreply.github.com>
2022-02-05 13:25:19 -0300

    TERCERA ENTREGA

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,11 @@
 import { ItemList } from "../itemList/itemList";
 import { Articulos } from "../../articulos/articulos";
 import { useEffect, useState } from "react";
+import img from "./esperar.jpg";
 
 export const ItemListContainer = ({ greeting }) => {
-  const [articulos, setProductos] = useState([]);
+  const [articulos, setArticulos] = useState([]);
+  const [espera, setEspera] = useState(false);
 
   const Tiempo = () => {
     return new Promise((resolve, reject) => {
@@ -14,18 +16,25 @@ export const ItemListContainer = ({ greeting }) => {
   };
 
   useEffect(() => {
+    setEspera(true);
     Tiempo()
       .then((res) => {
-        setProductos(res);
+        setArticulos(res);
+        console.log(res);
       })
       .catch((err) => {
         console.log(err);
+      })
+      .finally(() => {
+        setEspera(false);
       });
   }, []);
   return (
     <main className="main-Item container">
       <h2 className="h2-Item m-5 p-5">Menu</h2>
-      <ItemList articulos={articulos} />
+      <div>
+        {espera ? <img src={img} /> : <ItemList articulos={articulos} />}
+      </div>
     </main>
   );
 };
